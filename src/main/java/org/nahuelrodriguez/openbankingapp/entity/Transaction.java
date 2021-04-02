d5ecfee727fcab497d0d625bf328001461c11a50
Nahuel Rodriguez <nahuel>
2021-04-02 17:36:06 -0300

    feat: Added docker-compose.yml and Redis caching capability.

--- src/main/java/org/nahuelrodriguez/openbankingapp/entity/Transaction.java
+++ src/main/java/org/nahuelrodriguez/openbankingapp/entity/Transaction.java
@@ -2,10 +2,11 @@ package org.nahuelrodriguez.openbankingapp.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-public class Transaction {
+public class Transaction implements Serializable {
     private String type;
     private LocalDateTime date;
     private String accountNumber;
--- src/main/java/org/nahuelrodriguez/openbankingapp/service/impl/TransactionServiceImpl.java
+++ src/main/java/org/nahuelrodriguez/openbankingapp/service/impl/TransactionServiceImpl.java
@@ -5,6 +5,9 @@ import org.nahuelrodriguez.openbankingapp.adapter.TransactionApiClient;
 import org.nahuelrodriguez.openbankingapp.entity.Transaction;
 import org.nahuelrodriguez.openbankingapp.repository.MerchantDetailsRepository;
 import org.nahuelrodriguez.openbankingapp.service.TransactionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -14,20 +17,25 @@ import java.util.Collections;
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionApiClient transactionApiClient;
     private final MerchantDetailsRepository merchantDetailsRepository;
+    private final Logger log;
 
     public TransactionServiceImpl(final TransactionApiClient transactionApiClient,
                                   final MerchantDetailsRepository merchantDetailsRepository) {
         this.transactionApiClient = transactionApiClient;
         this.merchantDetailsRepository = merchantDetailsRepository;
+        this.log = LoggerFactory.getLogger(TransactionServiceImpl.class);
     }
 
     @Override
     @CircuitBreaker(name = "allTxsPerAccountNumber", fallbackMethod = "txsPerAccountNumberFallback")
+    @Cacheable(cacheNames = "txs", key = "#accountNumber")
     public Collection<Transaction> findAllByAccountNumber(final String accountNumber) {
+        log.info("Retrieving transactions from OpenAPI server for the account number: '{}'", accountNumber);
         final var transactions = transactionApiClient.getTransactions(accountNumber);
         transactions.stream()
                 .filter(tx -> tx.getMerchantName() != null)
                 .forEach(tx -> tx.setMerchantLogo(merchantDetailsRepository.getMerchantLogo(tx.getMerchantName())));
+        log.info("Returning '{}' transactions for the account number: '{}'", transactions.size(), accountNumber);
         return transactions;
     }
 
